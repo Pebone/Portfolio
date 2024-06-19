e7d7a53c56ecfdd13847f55fb212d7f71ba36e43
Davi Pereira <davipereiracardoso123@gmail.com>
2024-06-19 12:07:01 -0300

    responsive_v2

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,7 @@
 import Card_Projects from "./Cards_Projects/Card_Projects";
 import styles from "./Projects.module.scss";
+import Meteora from "../../assets/meteora.jpg";
+import Nlw_expert from "../../assets/nlw-expert.jpg"
 
 function Projects() {
   return (
@@ -13,49 +15,49 @@ function Projects() {
       </div>
       <div className={styles.cards}>
         <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
         ></Card_Projects>
         <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Nlw_expert}
+          project_type="Website"
+          project_name="Nlw-Expert"
+          project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
+        ></Card_Projects>
+        <Card_Projects
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
         ></Card_Projects>
         <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
         ></Card_Projects>
         <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
         ></Card_Projects>
         <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
         ></Card_Projects>
         <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
         ></Card_Projects>
         <Card_Projects
-          url="src\assets\meteora.jpg"
-          project_type="Website"
-          project_name="Meteora"
-          project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
-        ></Card_Projects>
-        <Card_Projects
-          url="src\assets\meteora.jpg"
+          url={Meteora}
           project_type="Website"
           project_name="Meteora"
           project_description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur asperiores praesentium itaque voluptate commodi, ipsum consequatur ducimus consectetur aperiam in nostrum eius ex molestiae fugiat rem hic cum exercitationem ratione."
